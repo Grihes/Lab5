533fe631201a60a6e810719b3e2027bba3706b44
Grihes <90863774+Grihes@users.noreply.github.com>
2021-11-14 23:46:19 +0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿
+namespace Lab5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.найтиКонтурыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьToolStripMenuItem,
+            this.найтиКонтурыToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1266, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // открытьToolStripMenuItem
+            // 
+            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(81, 24);
+            this.открытьToolStripMenuItem.Text = "Открыть";
+            this.открытьToolStripMenuItem.Click += new System.EventHandler(this.открытьToolStripMenuItem_Click);
+            // 
+            // найтиКонтурыToolStripMenuItem
+            // 
+            this.найтиКонтурыToolStripMenuItem.Name = "найтиКонтурыToolStripMenuItem";
+            this.найтиКонтурыToolStripMenuItem.Size = new System.Drawing.Size(241, 24);
+            this.найтиКонтурыToolStripMenuItem.Text = "Найти контур и площадь озера";
+            this.найтиКонтурыToolStripMenuItem.Click += new System.EventHandler(this.найтиКонтурыToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 92);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(557, 543);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(623, 92);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(565, 543);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = " ";
+            this.openFileDialog1.Filter = "PNG|*.png";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(623, 64);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(334, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(1035, 64);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(191, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1266, 647);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem найтиКонтурыToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu;
+using Emgu.CV;
+using Emgu.Util;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+
+namespace Lab5
+{
+
+    public partial class Form1 : Form
+    {
+        private Image<Bgr, byte> inputImage = null;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+            inputImage = new Image<Bgr, byte>(openFileDialog1.FileName);
+            pictureBox1.Image = inputImage.Bitmap;
+        }
+
+        private void найтиКонтурыToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Image<Gray, byte> outputImage = inputImage.Mul(1).SmoothGaussian(1).Convert<Gray, byte>().ThresholdBinaryInv(new Gray(50), new Gray(255));
+            VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint();
+            Mat hieratchy = new Mat();
+            Image<Gray, byte> blackBackground = new Image<Gray, byte>(inputImage.Width, inputImage.Height, new Gray(0));
+            CvInvoke.FindContours(outputImage, contours, hieratchy, Emgu.CV.CvEnum.RetrType.Tree, Emgu.CV.CvEnum.ChainApproxMethod.ChainApproxNone);
+            int n = 0;
+
+            double generalSquare = 0;
+            double imageSquare = 125000;
+            Random rnd = new Random();
+            int m = 10;
+            double[] LakeSquares = new double[m];
+            
+            double square = double.MinValue;
+            for (int i = 0; i < contours.Size; i++)
+            {
+                if (CvInvoke.ContourArea(contours[i]) > square)
+                {
+                    square = CvInvoke.ContourArea(contours[i]);
+                    n = i;
+                }
+            }
+            CvInvoke.DrawContours(blackBackground, contours,n, new MCvScalar(255, 0, 0));
+            pictureBox2.Image = blackBackground.Bitmap;
+            int count = 0;
+            int number = 170000;
+            for (int k = 0; k < m; k++)
+                {
+                 count = 0;
+                 for (int j = 0; j < number; j++)
+                  {
+                    if (CvInvoke.PointPolygonTest(contours[n], new PointF(rnd.Next(inputImage.Width), rnd.Next(inputImage.Height)), false) > 0)
+                    count++;
+                  }
+                  LakeSquares[k] = imageSquare * count / number;
+                  generalSquare += LakeSquares[k];
+                }
+             double LakeSquare = generalSquare / m;
+             textBox1.Text = "Square of Lake is: " + LakeSquare.ToString();
+            double sigma = 0;
+            double sumSquare=0;
+            for (int i = 0; i < m; i++)
+                sumSquare += LakeSquares[i] * LakeSquares[i];
+            sigma = Math.Sqrt(sumSquare / m - LakeSquare * LakeSquare);
+            textBox2.Text = "Sigma is: "+ sigma.ToString();
+        
+            }
+
+        }
+    }
+
